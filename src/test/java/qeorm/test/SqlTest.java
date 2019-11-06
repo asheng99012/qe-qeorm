9d1ca3a455bef02a91dc66a168d1644e48715090
郑建生 <zhengjiansheng@danke.com>
2019-11-07 01:24:34 +0800

    bugfix ,  单词边界兼容 `

--- src/test/java/qeorm/test/SqlTest.java
+++ src/test/java/qeorm/test/SqlTest.java
@@ -45,7 +45,7 @@ public class SqlTest {
     public void testKK() {
         String sql = " select id, rule_id, rule_name, regist_start_date, regist_end_date, effective_start_date, effective_end_date, create_at, update_at, status, create_user, update_user, remark, create_user_name, update_user_name, own_month from `rule_conf`\n" +
                 "     where\n" +
-                "     (regist_start_date <= {registStartDate} and regist_end_date >= {registStartDate})\n" +
+                "     (\nregist_start_date <= {registStartDate} and regist_end_date >= {registStartDate})\n" +
                 "      or (regist_start_date <= {registEndDate} and regist_end_date >= {registEndDate})";
         SqlConfig sqlConfig = new SqlConfig();
         sqlConfig.setSql(sql);
@@ -54,6 +54,21 @@ public class SqlTest {
 
         sql = "update rule_conf set a={a},b={b} where c={c}";
         sqlConfig = new SqlConfig();
+        sqlConfig.setSql(sql);
+        SqlConfigManager.parseSql(sqlConfig);
+
+        sql = "select * from user u where u.id={id}";
+        sqlConfig = new SqlConfig();
+        sqlConfig.setSql(sql);
+        SqlConfigManager.parseSql(sqlConfig);
+        System.out.println("==");
+    }
+
+    @Test
+    public void testUpdate(){
+        String sql="update `sell_staff` set `staff_no`={staffNo},`staff_name`={staffName},`job_status`={jobStatus},`city_code`={cityCode},`city_name`={cityName},`position`={position},`department`={department},`secondary_department`={secondaryDepartment},`tertiary_department`={tertiaryDepartment},`four_department`={fourDepartment},`join_date`={joinDate},`leave_date`={leaveDate},`is_seller`={isSeller},`job_level`={jobLevel},`guider`={guider},`mentor`={mentor},`supervisor`={supervisor},`illegal_commission`={illegalCommission},`other_commission`={otherCommission},`is_protected`={isProtected},`create_at`={createAt},`update_at`={updateAt},`create_user`={createUser},`create_user_name`={createUserName},`update_user`={updateUser},`update_user_name`={updateUserName},`yn`={yn},`version`={version},`performance_month_id`={performanceMonthId},`performance_month`={performanceMonth},`is_part_time`={isPartTime},`transfer_out_time`={transferOutTime},`transfer_out_position_after`={transferOutPositionAfter},`transfer_in_time`={transferInTime},`transfer_in_position_before`={transferInPositionBefore} where id={id} ";
+
+        SqlConfig sqlConfig = new SqlConfig();
         sqlConfig.setSql(sql);
         SqlConfigManager.parseSql(sqlConfig);
         System.out.println("==");
